0bdbf954002d279ed9b2163ffeed6bae3cb2a058
Victor Hugo Acosta Espinosa <victorhugo9602@hotmail.com>
2024-11-07 10:04:07 -0500

    working without compositizing and new images added

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,36 @@
 const App = () => {
 	return (
-		<div>
+		<>
 			<h1>Curso de react.</h1>
-		</div>
+			<div>
+				<h2>Colombia</h2>
+				<img
+					src='/assets/images/co.svg'
+					alt='bandera de colombia'
+					width={300}
+				/>
+			</div>
+			<div>
+				<h2>Canadá</h2>
+				<img src='/assets/images/ca.svg' alt='bandera de canada' width={300} />
+			</div>
+			<div>
+				<h2>España</h2>
+				<img src='/assets/images/es.svg' alt='bandera de españa' width={300} />
+			</div>
+			<div>
+				<h2>Francia</h2>
+				<img src='/assets/images/fr.svg' alt='bandera de francia' width={300} />
+			</div>
+			<div>
+				<h2>Portugal</h2>
+				<img
+					src='/assets/images/pt.svg'
+					alt='bandera de portugal'
+					width={300}
+				/>
+			</div>
+		</>
 	);
 };
 
